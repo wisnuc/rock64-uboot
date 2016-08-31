ac8c0b3b7df1a9469340f75361637ed4df857040
Kever Yang <kever.yang@rock-chips.com>
2016-08-31 16:40:00 +0800

    FROMLIST: board: evb-rk3399: add api to support dwc3 gadget
    This patch add board_usb_init() and interrupt callback
    for dwc3 gadget.
    
    Change-Id: I1ea19c0ec7d3fd14904e7fcc97c6d19221206762
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- board/rockchip/evb_rk3399/evb-rk3399.c
+++ board/rockchip/evb_rk3399/evb-rk3399.c
@@ -14,6 +14,8 @@
 #include <power/regulator.h>
 #include <spl.h>
 #include <u-boot/sha256.h>
+#include <usb.h>
+#include <dwc3-uboot.h>
 
 #define RK3399_CPUID_OFF  0x7
 #define RK3399_CPUID_LEN  0x10
@@ -210,5 +212,26 @@ void get_board_serial(struct tag_serialnr *serialnr)
 
 	serialnr->high = (u32)(serial >> 32);
 	serialnr->low = (u32)(serial & 0xffffffff);
+}
+#endif
+
+#ifdef CONFIG_USB_DWC3
+static struct dwc3_device dwc3_device_data = {
+	.maximum_speed = USB_SPEED_HIGH,
+	.base = 0xfe800000,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.index = 0,
+	.dis_u2_susphy_quirk = 1,
+};
+
+int usb_gadget_handle_interrupts(void)
+{
+	dwc3_uboot_handle_interrupt(0);
+	return 0;
+}
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	return dwc3_uboot_init(&dwc3_device_data);
 }
 #endif
