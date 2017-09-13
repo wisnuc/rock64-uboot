9e0339136352cff79d9d6a0ea868ab0ddc297532
Elaine Zhang <zhangqing@rock-chips.com>
2017-09-13 18:04:51 +0800

    power: pmic: rk816: support rk816 pmic
    Add Rockchip pmic rk816 support.
    
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- drivers/power/pmic/rk8xx.c
+++ drivers/power/pmic/rk8xx.c
@@ -98,6 +98,7 @@ static struct dm_pmic_ops rk8xx_ops = {
 
 static const struct udevice_id rk8xx_ids[] = {
 	{ .compatible = "rockchip,rk808" },
+	{ .compatible = "rockchip,rk816" },
 	{ .compatible = "rockchip,rk818" },
 	{ }
 };
