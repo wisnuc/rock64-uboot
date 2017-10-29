linhw3/uboot

rockchip: add BOOT_LOADER support for boot mode
Rockchip using rockusb for flash emmc via USB, people
use 'reboot loader' in kernel and then want to get a rockusb
device in bootloader for firmware update.

Change-Id: I16480f07761c2199443373aa54d458ca0851b708
Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

arch/arm/mach-rockchip/rk322x-board.c
@@ -36,6 +36,10 @@ static void setup_boot_mode(void)
 		printf("enter UMS!\n");
 		setenv("preboot", "setenv preboot; ums mmc 0");
 		break;
+	case BOOT_LOADER:
+		printf("enter Rockusb!\n");
+		setenv("preboot", "setenv preboot; rockusb 0 mmc 0");
+		break;
 	}
 }
 

wisnuc/rock64-uboot

arch/arm/mach-rockchip/rk322x-board.c
@@ -37,6 +37,10 @@ static void setup_boot_mode(void)
 		printf("enter UMS!\n");
 		env_set("preboot", "setenv preboot; ums mmc 0");
 		break;
+	case BOOT_LOADER:
+		printf("enter Rockusb!\n");
+		env_set("preboot", "setenv preboot; rockusb 0 mmc 0");
+		break;
 	}
 }
 